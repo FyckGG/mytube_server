b050fc1a4c8fc4df04c1e61e1663157a9ba5844d
DmitryFaber <mistertwister698@gmail.com>
2022-11-08 14:26:12 +0700

    add video model

---
+++ models/Video.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const Video = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  video_name: { type: String, required: true },
+  video_directory: { type: String, required: true },
+  video_description: { type: String },
+  is_public: { type: Boolean, default: true },
+  video_subject: { type: String, required: true },
+  upload_date: { type: Date, default: Date.now },
+});
+
+module.exports = new model("Video", Video);
