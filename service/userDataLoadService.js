92676bad6f8f8af99c8934277bf57ac3e433c55a
DmitryFaber <mistertwister698@gmail.com>
2022-11-16 18:36:59 +0700

    create array for video min in dataloadservice

--- service/userDataLoadService.js
+++ service/userDataLoadService.js
@@ -1,5 +1,7 @@
 const UserAvatar = require("./../models/UserAvatar");
 const Video = require("./../models/Video");
+const VideoThumbnail = require("./../models/VideoThumbnail");
+const UserVideo = require("./../VideoClasses/UserVideo");
 
 class TokenService {
   async findAvatar(id) {
@@ -11,8 +13,24 @@ class TokenService {
 
   async loadUserVideos(user_id) {
     try {
+      const user_videos_arr = [];
       const videos = await Video.find({ user: user_id });
-      return videos;
+
+      for (let i = 0; i < videos.length; i++) {
+        const thumbnails = await VideoThumbnail.findOne({
+          video: videos[i]._id,
+        });
+        const userVideo = new UserVideo(
+          videos[i]._id,
+          videos[i].video_name,
+          videos[i].video_duration,
+          228,
+          `${thumbnails.thumbnail_directory}/${thumbnails.thumbnail_name}`
+        );
+        user_videos_arr.push(userVideo);
+      }
+
+      return user_videos_arr;
     } catch (e) {
       console.log("Ошибка при получении видео: " + e);
     }
