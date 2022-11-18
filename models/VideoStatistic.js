d91cda7e6939d02fb679fed06ec4864535aedea0
DmitryFaber <mistertwister698@gmail.com>
2022-11-18 21:23:51 +0700

    add new model for video statistics

--- models/VideoStatistic.js
+++ models/VideoStatistic.js
@@ -2,10 +2,10 @@ const { Schema, model } = require("mongoose");
 
 const VideoStatistics = new Schema({
   video: { type: Schema.Types.ObjectId, ref: "Video" },
-  count_of_like: { type: Number, required: true },
-  count_of_dislike: { type: Number, required: true },
-  count_of_views: { type: Number, required: true },
-  count_of_comments: { type: Number, required: true },
+  count_of_like: { type: Number, required: true, default: 0 },
+  count_of_dislike: { type: Number, required: true, default: 0 },
+  count_of_views: { type: Number, required: true, default: 0 },
+  count_of_comments: { type: Number, required: true, default: 0 },
 });
 
 module.exports = new model("VideoStatistics", VideoStatistics);
