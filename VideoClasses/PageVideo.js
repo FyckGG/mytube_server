6658506c2ef4a68318bfc79905ab417481b563d4
DmitryFaber <mistertwister698@gmail.com>
2022-12-30 14:29:50 +0700

    add new class for render video on main page

---
+++ VideoClasses/PageVideo.js
@@ -0,0 +1,32 @@
+module.exports = class PageVideo {
+  video_id;
+  video_name;
+  video_preview;
+  channel_id;
+  channel_name;
+  channel_avatar;
+  count_views;
+  video_duration;
+  video_date;
+  constructor(
+    video_id,
+    video_name,
+    video_preview,
+    channel_id,
+    channel_name,
+    channel_avatar,
+    count_views,
+    video_duration,
+    video_date
+  ) {
+    this.video_id = video_id;
+    this.video_name = video_name;
+    this.video_preview = video_preview;
+    this.channel_id = channel_id;
+    this.channel_name = channel_name;
+    this.channel_avatar = channel_avatar;
+    this.count_views = count_views;
+    this.video_duration = video_duration;
+    this.video_date = video_date;
+  }
+};
