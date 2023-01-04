19a0fb5f5bd3afb7d6e4fdca1430a52fdac7df19
DmitryFaber <mistertwister698@gmail.com>
2023-01-04 15:15:15 +0700

    add service for creating video for page

---
+++ otherServices/getPageVideo.js
@@ -0,0 +1,34 @@
+const PageVideo = require("./../VideoClasses/PageVideo");
+const VideoThumbnail = require("./../models/VideoThumbnail");
+const User = require("./../models/User");
+const VideoStatistic = require("./../models/UserStatistic");
+const UserAvatar = require("./../models/UserAvatar");
+
+const getPageVIdeo = async (video) => {
+  let video_preview = await VideoThumbnail.findOne({ video: video._id });
+  if (!video_preview) throw new Error("Не удалось получить превью видео.");
+  let video_channel = await User.findById(video.user);
+  if (!video_channel)
+    throw new Error("Не удалось получить пользователя, загрузившего видео.");
+  let channel_avatar = await UserAvatar.findOne({
+    user: video_channel._id,
+  });
+  if (!channel_avatar)
+    throw new Error("Не удалось получить аватар пользователя.");
+  let video_stats = await VideoStatistic.findOne({ video: video._id });
+  if (!video_stats) throw new Error("Не удалось получить статистику видео.");
+  const video_for_page = new PageVideo(
+    video._id,
+    video.video_name,
+    `${video_preview.thumbnail_directory}/${video_preview.thumbnail_name}`,
+    video_channel._id,
+    video_channel.login,
+    `${channel_avatar.avatar_dir}${channel_avatar.avatar_name}`,
+    video_stats.count_of_views,
+    video.video_duration,
+    video.upload_date
+  );
+  return video_for_page;
+};
+
+module.exports = getPageVIdeo;
