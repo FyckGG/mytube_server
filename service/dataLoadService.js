5d9a2f3ebb87357cd3ea25301923bb29b2f86f8e
DmitryFaber <mistertwister698@gmail.com>
2023-01-04 15:16:36 +0700

    add getPageVideo in dataLoadService

--- service/dataLoadService.js
+++ service/dataLoadService.js
@@ -6,6 +6,7 @@ const UserAvatar = require("./../models/UserAvatar");
 const VideoThumbnail = require("./../models/VideoThumbnail");
 const PageComment = require("./../CommentClasses/PageComment");
 const PageVideo = require("./../VideoClasses/PageVideo");
+const getPageVIdeo = require("./../otherServices/getPageVideo");
 
 class DataLoadService {
   async loadComments(video_id, comment_count) {
@@ -54,33 +55,34 @@ class DataLoadService {
     const page_videos = [];
     const videos = await Video.find();
     for (let video of videos) {
-      let video_preview = await VideoThumbnail.findOne({ video: video._id });
-      if (!video_preview) throw new Error("Не удалось получить превью видео.");
-      let video_channel = await User.findById(video.user);
-      if (!video_channel)
-        throw new Error(
-          "Не удалось получить пользователя, загрузившего видео."
-        );
-      let channel_avatar = await UserAvatar.findOne({
-        user: video_channel._id,
-      });
-      if (!channel_avatar)
-        throw new Error("Не удалось получить аватар пользователя.");
-      let video_stats = await VideoStatistic.findOne({ video: video._id });
-      if (!video_stats)
-        throw new Error("Не удалось получить статистику видео.");
-      const video_for_page = new PageVideo(
-        video._id,
-        video.video_name,
-        `${video_preview.thumbnail_directory}/${video_preview.thumbnail_name}`,
-        video_channel._id,
-        video_channel.login,
-        `${channel_avatar.avatar_dir}${channel_avatar.avatar_name}`,
-        video_stats.count_of_views,
-        video.video_duration,
-        video.upload_date
-      );
-      //console.log(video_for_page);
+      // let video_preview = await VideoThumbnail.findOne({ video: video._id });
+      // if (!video_preview) throw new Error("Не удалось получить превью видео.");
+      // let video_channel = await User.findById(video.user);
+      // if (!video_channel)
+      //   throw new Error(
+      //     "Не удалось получить пользователя, загрузившего видео."
+      //   );
+      // let channel_avatar = await UserAvatar.findOne({
+      //   user: video_channel._id,
+      // });
+      // if (!channel_avatar)
+      //   throw new Error("Не удалось получить аватар пользователя.");
+      // let video_stats = await VideoStatistic.findOne({ video: video._id });
+      // if (!video_stats)
+      //   throw new Error("Не удалось получить статистику видео.");
+      // const video_for_page = new PageVideo(
+      //   video._id,
+      //   video.video_name,
+      //   `${video_preview.thumbnail_directory}/${video_preview.thumbnail_name}`,
+      //   video_channel._id,
+      //   video_channel.login,
+      //   `${channel_avatar.avatar_dir}${channel_avatar.avatar_name}`,
+      //   video_stats.count_of_views,
+      //   video.video_duration,
+      //   video.upload_date
+      // );
+      const video_for_page = await getPageVIdeo(video);
+
       page_videos.push(video_for_page);
     }
     return page_videos;
