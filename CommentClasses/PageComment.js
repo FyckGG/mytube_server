22b480d68ce98251b67e0c82170aabed875025e0
DmitryFaber <mistertwister698@gmail.com>
2023-01-12 18:29:16 +0700

    add comment id for commentPage

--- CommentClasses/PageComment.js
+++ CommentClasses/PageComment.js
@@ -1,11 +1,20 @@
 module.exports = class PageComment {
+  comment_id;
   user_id;
   user_name;
   user_avatar;
   comment_text;
   comment_date;
 
-  constructor(user_id, user_name, user_avatar, comment_text, comment_date) {
+  constructor(
+    comment_id,
+    user_id,
+    user_name,
+    user_avatar,
+    comment_text,
+    comment_date
+  ) {
+    this.comment_id = comment_id;
     this.user_id = user_id;
     this.user_name = user_name;
     this.user_avatar = user_avatar;
