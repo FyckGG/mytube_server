97e300ce9b206dbe83e5871f6d7c12351ea7a5df
DmitryFaber <mistertwister698@gmail.com>
2023-01-25 18:36:02 +0700

    add getting videoTags for edit

--- service/videoDataLoadService.js
+++ service/videoDataLoadService.js
@@ -1,18 +1,20 @@
 const Video = require("./../models/Video");
+const VideoTags = require("./../models/VideoTags");
 
 class VideoDataLoadService {
   async getVideoForEdit(video_id, user_id) {
     const video = await Video.findById(video_id);
     if (!video) throw new Error("Видео не найдено");
-    console.log(user_id);
-    console.log(video.user);
+
     if (video.user != user_id)
       throw new Error("Пользователь не имеет права редактировать это видео");
+    const video_tags = await VideoTags.findOne({ video: video_id });
     return {
       name: video.video_name,
       description: video.video_description,
       access_type: video.is_public,
       subject: video.video_subject,
+      tags: video_tags,
     };
   }
 }
